5e555080c465024519cd3b7549d03653a256d73a
prathyu116 <prathyuprasad116@gmail.com>
2025-03-22 13:08:52 +0530

    added soln for var productExceptSelf = function (nums) {     const arr = []     const prefixArr = []     const suffixArr = []     for (let i = 0; i < nums.length; i++) {         if (i === 0) {             prefixArr[i] = 1
            } else {
                prefixArr[i] = prefixArr[i - 1] * nums[i - 1]
            }
    
        }
        for (var i = nums.length - 1; i >= 0; i--) {
            if (i === nums.length - 1) {
                suffixArr[i] = 1
            } else {
                suffixArr[i] = suffixArr[i + 1] * nums[i + 1]
            }
        }
        for (let i = 0; i < nums.length; i++) {
            arr[i] = prefixArr[i] * suffixArr[i]
    
        };
        return arr
    }
    
        console.log(productExceptSelf([1, 2, 3, 4])); // [24, 12, 8, 6]
    
    //
    
    /*
    rightProduct__index[i]___leftProduct
    then rightProduct*leftProduct
    
    https://youtu.be/TW2m8m_FNJE
    
    */

---
+++ Challenge-5/Product of Array Except Self.js
@@ -0,0 +1,31 @@
+var productExceptSelf = function (nums) {
+    const arr = []
+    const prefixArr = []
+    const suffixArr = []
+    for (let i = 0; i < nums.length; i++) {
+        if (i === 0) {
+            prefixArr[i] = 1
+
+
+        } else {
+            prefixArr[i] = prefixArr[i - 1] * nums[i - 1]
+        }
+
+    }
+    for (var i = nums.length - 1; i >= 0; i--) {
+        if (i === nums.length - 1) {
+            suffixArr[i] = 1
+        } else {
+            suffixArr[i] = suffixArr[i + 1] * nums[i + 1]
+        }
+    }
+    for (let i = 0; i < nums.length; i++) {
+        arr[i] = prefixArr[i] * suffixArr[i]
+
+
+    };
+    return arr
+}
+
+console.log(productExceptSelf([1, 2, 3, 4])); 
+
