d19c9536fe0909ad96fbf23ccf1a347a11614d0b
prathyu116 <prathyuprasad116@gmail.com>
2025-03-26 23:31:42 +0530

    Added solution for Search in Rotated Sorted Array

---
+++ Challenge-10/Search in Rotated Sorted Array.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+function getPivot(arr) {
+    var s = 0, e = arr.length - 1;
+    while (s < e) {
+        var mid = Math.floor(s + (e - s) / 2)
+
+        if (arr[mid] >= arr[0]) {
+            s = mid + 1
+        } else {
+            e = mid
+        }
+    }
+    return s;
+}
+
+function binarySearch(arr, start, end, key) {
+    var s = start, e = end;
+
+    while (s <= e) {
+        var mid = Math.floor(s + (e - s) / 2)
+        if (arr[mid] == key) {
+            return mid
+        }
+        if (key > arr[mid]) {
+            s = mid + 1;
+        } else {
+            e = mid - 1
+        }
+    }
+    return -1;
+}
+var search = function (arr, target) {
+    var n = arr.length
+    var pivot = getPivot(arr)
+
+    if (target >= arr[pivot] && target <= arr[n - 1]) {
+        return binarySearch(arr, pivot, n - 1, target)
+    } else {
+        return binarySearch(arr, 0, pivot - 1, target)
+    }
+
+};
