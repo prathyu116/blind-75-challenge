423ca0a54d0b8ea0dabb09524d8a5df72a0dbba7
prathyu116 <prathyuprasad116@gmail.com>
2025-04-01 12:21:17 +0530

    aded soln for reverse bit

---
+++ Challenge-16/Reverse Bits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ * https://leetcode.com/problems/reverse-bits/
+ */
+
+var reverseBits = function (n) {
+    let result = n.toString(2)
+    result = result.padStart(32, '0');
+    let reversedString = result.split('').reverse().join('');
+    let ans = parseInt(reversedString, 2)
+    return ans
+};
